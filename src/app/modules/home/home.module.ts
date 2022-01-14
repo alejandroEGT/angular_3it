716670e3813b5c2971342f2b798844233561d1b2
alejandroEGT <alejandro.e.g.t29@gmail.com>
2022-01-14 06:41:56 -0300

    test

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,7 +6,7 @@ import { IndicadoresComponent } from './pages/indicadores/indicadores.component'
 import { MenuComponent } from './components/menu/menu.component';
 import { PadreComponent } from './pages/padre/padre.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { PipesModule } from 'src/app/shared/pipes/PipesModule';
+import { PipesModule } from 'src/app/shared/pipes/pipesModule';
 import { DetalleIndicadorComponent } from './components/detalle-indicador/detalle-indicador.component';
 import { GraficoComponent } from './components/grafico/grafico.component';
 import { DetalleValoresIndicadorComponent } from './pages/detalle-valores-indicador/detalle-valores-indicador.component';
